f279d33b4f47754ecef365f45c04c4e56107c768
Rafael Benevides <rafabene@gmail.com>
2025-05-15 16:26:57 -0300

    Iniciado implementação gRPC

--- cleanarch/internal/infra/grpc/service/order_service.go
+++ cleanarch/internal/infra/grpc/service/order_service.go
@@ -10,12 +10,39 @@ import (
 type OrderService struct {
 	pb.UnimplementedOrderServiceServer
 	CreateOrderUseCase usecase.CreateOrderUseCase
+	ListOrderUseCase   usecase.ListOrderUseCase
 }
 
 func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase) *OrderService {
 	return &OrderService{
 		CreateOrderUseCase: createOrderUseCase,
 	}
+}
+
+func ListOrderUseCase(listOrderUserCase usecase.ListOrderUseCase) *OrderService {
+	return &OrderService{
+		ListOrderUseCase: listOrderUserCase,
+	}
+}
+
+func (s *OrderService) ListOrder(ctx context.Context) (*pb.ListOrdersResponse, error) {
+	orders, err := s.ListOrderUseCase.Execute()
+	if err != nil {
+		return nil, err
+	}
+	var response = pb.ListOrdersResponse{
+		Orders: make([]*pb.CreateOrderResponse, len(orders.OrderListDTO)),
+	}
+	for i, order := range orders.OrderListDTO {
+		response.Orders[i] = &pb.CreateOrderResponse{
+			Id:         order.ID,
+			Price:      float32(order.Price),
+			Tax:        float32(order.Tax),
+			FinalPrice: float32(order.FinalPrice),
+		}
+	}
+	return &response, nil
+
 }
 
 func (s *OrderService) CreateOrder(ctx context.Context, in *pb.CreateOrderRequest) (*pb.CreateOrderResponse, error) {
